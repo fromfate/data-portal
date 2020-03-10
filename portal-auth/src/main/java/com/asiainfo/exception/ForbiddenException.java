eec06a2ebcd7490d473e5293bc7e64ac4c986665
Li Heng <liheng@asiainfo.com>
2020-03-10 12:38:56 +0800

    配置修改

--- portal-auth/src/main/java/com/asiainfo/exception/ForbiddenException.java
+++ portal-auth/src/main/java/com/asiainfo/exception/ForbiddenException.java
@@ -1,9 +1,5 @@
 package com.asiainfo.exception;
 
-/**
- * @author: zhagnjunqian
- * @create: 2019-07-31 14:25
- **/
 public class ForbiddenException extends Exception {
 
     public ForbiddenException(String message) {
