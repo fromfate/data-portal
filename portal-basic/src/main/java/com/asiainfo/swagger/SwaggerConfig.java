de46ba23558489ce6b479fff88d1e0d3326ab951
Li Heng <liheng@asiainfo.com>
2020-03-10 12:10:46 +0800

    配置修改

--- portal-basic/src/main/java/com/asiainfo/swagger/SwaggerConfig.java
+++ portal-basic/src/main/java/com/asiainfo/swagger/SwaggerConfig.java
@@ -1,24 +1,30 @@
 package com.asiainfo.swagger;
 
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    private static String SPLITOR = ";";
+
     @Bean
     public Docket userApi() {
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.asiainfo.controller"))//过滤的接口
+                .apis(basePackage("com.asiainfo.controller"+SPLITOR + "com.asiainfo.monitor.controller"))//过滤的接口
                 .paths(PathSelectors.any())
                 .build();
         return docket;
@@ -30,6 +36,27 @@ public class SwaggerConfig {
                 .description("门户集成接口文档")
                 .version("1.0.1")
                 .build();
+    }
+
+    public static Predicate<RequestHandler> basePackage(final String basePackage) {
+        return input -> declaringClass(input).transform(handlerPackage(basePackage)).or(true);
+    }
+
+    private static Function<Class<?>, Boolean> handlerPackage(final String basePackage)     {
+        return input -> {
+            // 循环判断匹配
+            for (String strPackage : basePackage.split(SPLITOR)) {
+                boolean isMatch = input.getPackage().getName().startsWith(strPackage);
+                if (isMatch) {
+                    return true;
+                }
+            }
+            return false;
+        };
+    }
+
+    private static Optional<? extends Class<?>> declaringClass(RequestHandler input) {
+        return Optional.fromNullable(input.declaringClass());
     }
 
 }
